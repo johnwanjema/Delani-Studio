6c815e5580f899b72efca4888e67fe1cab17fbae
johnwanjema <jonwanjema@gmail.com>
2019-05-04 13:10:48 +0300

    adds js function to toggle the what we do section

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $(".img1").hover(function () {
+        alert("weweweweqwertyuiopfghjk")
+        $(".design").toggle();
+    });
+    $(".img2").hover(function () {
+        alert("q2q3q3q4q5q5q56q6")
+        $(".development").toggle();
+    });
+    $(".img3").hover(function () {
+        alert("w1234567876544567834563456765434567834567k")
+        $(".product").toggle();
+    });
+
+
+
+
+
+
+});
