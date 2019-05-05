197131cc651f1163e6ac0e6a97799f82ca4c85cf
johnwanjema <jonwanjema@gmail.com>
2019-05-05 19:28:41 +0300

    added hover effect over portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,29 @@
 $(document).ready(function () {
-    $(".img1").hover(function () {
+    $(".img1").click(function () {
         $(".d").toggle();
         $(".design").toggle();
     });
-    $(".img2").hover(function () {
+    $(".img2").click(function () {
         $(".d1").toggle();
         $(".development").toggle();
     });
-    $(".img3").hover(function () {
+    $(".img3").click(function () {
         $(".d2").toggle();
         $(".product").toggle();
-    });
-
-
-
-
-
-
+    });    
 });
+
+
+$(document).ready(function(){
+  
+    $('.show').hover(function () {
+      $(this).animate({opacity:'1'});
+      },
+      function () {
+      $(this).animate({opacity:'0'});
+  });
+  });
+
+
+
+
