4a0dc090da058d871f2ba799fde4b46123830084
johnwanjema <jonwanjema@gmail.com>
2019-05-06 10:53:43 +0300

    edits jss

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,6 @@ $(document).ready(function () {
         $(".img1").toggle();
         $(".design").hide();
     });
-
-
-
     $(".img2").click(function () {
         $(".img2").hide();
         $(".development").toggle();
@@ -21,24 +18,25 @@ $(document).ready(function () {
     $(".img3").click(function () {
         $(".img3").hide();
         $(".product").toggle();
-    });   
+    });
     $(".product").click(function () {
         $(".img3").toggle();
         $(".product").hide();
-    }); 
+    });
+    $("#submit1").click(function () {
+        alert("your message has been received")
+       
+    });
 });
 
+$(document).ready(function () {
 
-$(document).ready(function(){
-  
     $('.show').hover(function () {
-      $(this).animate({opacity:'1'});
-      },
-      function () {
-      $(this).animate({opacity:'0'});
-  });
-  });
-
-
+        $(this).animate({ opacity: '1' });
+    },
+        function () {
+            $(this).animate({ opacity: '0' });
+        });
+});
 
 
